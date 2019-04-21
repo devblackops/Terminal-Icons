15ee1bf52bbbff6d45301df28ba9a1a3856f6a67
Brandon Olin <brandon@devblackops.io>
2019-04-20 21:06:35 -0700

    Install psake as part of build script
    PSDepend doesn't support installing prerelease versions (yet)

--- build.ps1
+++ build.ps1
@@ -19,9 +19,12 @@ if ($Bootstrap.IsPresent) {
     Get-PackageProvider -Name Nuget -ForceBootstrap | Out-Null
     Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
     if (-not (Get-Module -Name PSDepend -ListAvailable)) {
-        Install-module -Name PSDepend -Repository PSGallery
+        Install-Module -Name PSDepend -Repository PSGallery
     }
-    Import-Module -Name PSDepend -Verbose:$false
+    if (-not (Get-Module -Name psake -ListAvailable) | Where-Object -Version -eq 4.8.0) {
+        Install-Module -Name psake -RequiredVersion '4.8.0-alpha' -AllowPrerelease -Repository PSGallery
+    }
+    Import-Module -Name psake, PSDepend -Verbose:$false
     Invoke-PSDepend -Path './requirements.psd1' -Install -Import -Force -WarningAction SilentlyContinue
 }
 
