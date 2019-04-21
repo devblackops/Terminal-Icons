088ab99e7368ba5a16c8769e5427a294504195b9
Brandon Olin <brandon@devblackops.io>
2019-04-20 20:45:23 -0700

    Use correct PowerShellBuild preference variable

--- psakeFile.ps1
+++ psakeFile.ps1
@@ -2,7 +2,7 @@ properties {
     # Disable "compiling" module into monolithinc PSM1.
     # This modifies the default behavior from the "Build" task
     # in the PowerShellBuild shared psake task module
-    $CompileModule = $false
+    $PSBPreference.Build.CompileModule = $false
 }
 
 task default -depends Test
