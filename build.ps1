ce6892168ec6344de8d506c9da9197181a22926f
Brandon Olin <brandon@devblackops.io>
2019-06-11 07:56:49 -0700

    Remove installs of pre-release build dependencies

--- build.ps1
+++ build.ps1
@@ -19,15 +19,8 @@ if ($Bootstrap.IsPresent) {
     Get-PackageProvider -Name Nuget -ForceBootstrap | Out-Null
     Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
     if (-not (Get-Module -Name PSDepend -ListAvailable)) {
-        Install-Module -Name PSDepend -Repository PSGallery
+        Install-Module -Name PSDepend -Repository PSGallery -Scope CurrentUser
     }
-    if (-not (Get-Module -Name psake -ListAvailable | Where-Object Version -eq 4.8.0).Count -eq 1) {
-        Install-Module -Name psake -RequiredVersion '4.8.0-alpha' -AllowPrerelease -Repository PSGallery
-    }
-    if (-not (Get-Module -Name PowerShellBuild -ListAvailable | Where-Object Version -eq 0.3.0).Count -eq 1) {
-        Install-Module -Name PowerShellBuild -RequiredVersion '0.3.0-beta2' -AllowPrerelease -Repository PSGallery
-    }
-    Import-Module -Name PowerShellBuild, psake, PSDepend -Verbose:$false
     Invoke-PSDepend -Path './requirements.psd1' -Install -Import -Force -WarningAction SilentlyContinue
 }
 
