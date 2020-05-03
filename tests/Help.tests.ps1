e12d5aa8ed9df85320b4b567274f6aed81216318
Brandon Olin <brandon@devblackops.io>
2020-05-03 13:54:08 -0700

    Remove workflows for pwsh 7 compat

--- tests/Help.tests.ps1
+++ tests/Help.tests.ps1
@@ -10,7 +10,7 @@ $outputModVerDir = Join-Path -Path $outputModDir -ChildPath $manifest.Version
 # Remove all versions of the module from the session. Pester can't handle multiple versions.
 Get-Module $env:BHProjectName | Remove-Module -Force
 Import-Module -Name (Join-Path -Path $outputModVerDir -ChildPath "$($env:BHProjectName).psd1") -Verbose:$false -ErrorAction Stop
-$commands = Get-Command -Module (Get-Module $env:BHProjectName) -CommandType Cmdlet, Function, Workflow  # Not alias
+$commands = Get-Command -Module (Get-Module $env:BHProjectName) -CommandType Cmdlet, Function
 
 ## When testing help, remember that help is cached at the beginning of each session.
 ## To test, restart session.
