856cd2743d8381cc0e72d29652b8a1a53b2c173e
Brandon Olin <brandon@devblackops.io>
2020-10-22 23:37:58 -0700

    Add act task to test GH Actions locally

--- psakeFile.ps1
+++ psakeFile.ps1
@@ -10,3 +10,21 @@ properties {
 task default -depends Test
 
 task Pester -FromModule PowerShellBuild -Version '0.4.0' -preaction {$script:modPath = $PSBPreference.General.ModuleManifestPath; Import-Module $script:modPath}
+
+task InstallAct {
+    if (-not (Get-Command -Name act -CommandType Application -ErrorAction SilentlyContinue)) {
+        if ($IsWindows) {
+            choco install act-cli
+        } elseif ($IsLinux) {
+            curl https://raw.githubusercontent.com/nektos/act/master/install.sh | sudo bash
+        } elseIf ($IsMacOS) {
+            brew install nektos/tap/act
+        }
+    } else {
+        'act already installed'
+    }
+}
+
+task TestGHAction -depends InstallAct {
+    act -j test -P ubuntu-latest=nektos/act-environments-ubuntu:18.04
+}
