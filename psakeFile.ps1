be1e6cdba75760be421ef4822582254dffbb3df4
Brandon Olin <brandon@devblackops.io>
2021-04-20 21:59:45 -0700

    Exclude PSUseBOMForUnicodeEncodedFile rule
    We don't want a BOM on the PSM1
    See: https://github.com/PowerShell/PSScriptAnalyzer/issues/810

--- psakeFile.ps1
+++ psakeFile.ps1
@@ -1,12 +1,18 @@
 properties {
+    # Build settings
     $PSBPreference.Build.CompileModule   = $true
     $PSBPreference.Build.CopyDirectories = @('Data')
     $PSBPreference.Build.CompileHeader   = @'
 using namespace System.Management.Automation
 using namespace System.Collections.ObjectModel
 '@
-    $PSBPreference.Test.ImportModule = $true
-    $PSBPreference.Test.OutputFile   = "$($PSBPreference.Build.OutDir)/testResults.xml"
+
+    # Test settings
+    $PSBPreference.Test.ImportModule                =   $true
+    $PSBPreference.Test.OutputFile                  = [IO.Path]::Combine($PSBPreference.Build.OutDir, 'testResults.xml')
+    $PSBPreference.Test.ScriptAnalysis.SettingsPath = [IO.Path]::Combine($PSBPreference.Test.RootDir, 'ScriptAnalyzerSettings.psd1')
+
+    # Publish settings
     if ($galleryApiKey) {
         $PSBPreference.Publish.PSRepositoryApiKey = $galleryApiKey.GetNetworkCredential().password
     }
